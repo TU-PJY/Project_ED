c0baed5478b2331c87dfceff0afc5cbffed9c65f
TU-PJY <viska0319@icloud.com>
2025-01-19 15:08:15 +0900

    도형 리소스와 폰트 리소스 추가, ignore 업데이트

--- mata_SDK/Config.h
+++ mata_SDK/Config.h
@@ -16,7 +16,7 @@ constexpr bool FULL_SCREEN_OPTION = false;
 
 //////////////////////////////////// SDK options
 // intro screen option
-constexpr bool ENABLE_INTRO_SCREEN = true;
+constexpr bool ENABLE_INTRO_SCREEN = false;
 
 // frame limits option
 // If this option is set to 0, the system will run at maximum framerate.
